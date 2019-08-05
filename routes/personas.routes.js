1bbc9948a1090080483d050b5ca0972cf29c65e5
Daniel Pat <danielpat6@gmail.com>
2019-08-05 13:20:23 -0500

    Cambios

--- routes/personas.routes.js
+++ routes/personas.routes.js
@@ -25,4 +25,26 @@ router.post('/crear',async(req, res)=>{
     res.redirect('/')
 })
 
+router.get('/editar/:id', async(req, res)=>{
+    const persona = await Personas.findById(req.params.id)
+    res.render('Editar',{persona})
+})
+
+router.post('/editar/:id',async(req, res)=>{
+    const { nombre, edad, telefono, hijos, estadocivil, Libros, Deportes, Musica, Otros } = req.body;
+    const intereses = [
+        { key:"Libros" , value:(Libros) ? true: false},
+        { key:"Deportes" , value:(Deportes) ? true: false},
+        { key:"Musica" , value:(Musica) ? true: false},
+        { key:"Otros" , value:(Otros) ? true: false},
+    ]
+    await Personas.findByIdAndUpdate(req.params.id,{nombre,edad, telefono, hijos:(hijos == "Si") ? true:false,estadocivil, intereses})
+    res.redirect('/')
+})
+
+router.get('/eliminar/:id', async(req, res)=>{
+    await Personas.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+})
+
 module.exports = router;
